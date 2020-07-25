1f6171800c42f6f8dae7af63f11ed1403f4ab4ef
ktotheH <kcjhill1234@msn.com>
2020-07-25 12:06:32 -0600

    heroku deployment setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const express = require("express")
+const express = require("express");
 const PORT = process.env.PORT || 3002 
 const app = express() 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+}
+app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now listening on port ${PORT}`)
 }); 
+
