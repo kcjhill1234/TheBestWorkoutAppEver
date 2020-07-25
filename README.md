cbee57aae605490ff322207fb490bfbddf1528a7
Senad <senadutah@gmail.com>
2020-07-24 18:17:14 -0600

    added api

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ to start development:
 ```
 npm start
 ```
+command runs index.js, we are using nodemon with this command, also added inspect tool for chrome://inspect
+```
+npm run dev
+```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const PORT = process.env.PORT || 3002 
 const app = express() 
-
+const api = require ("./api/api")
+// api();
 app.listen(PORT, () => {
     console.log(`API server now listening on port ${PORT}`)
 }); 
