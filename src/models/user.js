91f02c9333f7c5ce05f2ab5bcee57b2a6a0f24a4
ktotheH <kcjhill1234@msn.com>
2020-07-25 15:15:30 -0600

    added user models

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {type: String, require: true},
+    lastName: {type: String, require: true},
+    email: {type: String, require: true},
+    userName: {type: String, require: true},
+    password: {type: String, require: true},
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
