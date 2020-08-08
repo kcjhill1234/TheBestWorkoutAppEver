fa8ee1e24ec83e1a44f1e64dd3282f20cc5813c8
Senad <senadutah@gmail.com>
2020-08-08 10:37:24 -0600

    added readme and quotes

--- README.md
+++ README.md
@@ -16,4 +16,26 @@ to run tests
 npm test
 ```
 
+# Hot to use the best workout App?
+
+## On home page user is able to log in and follow his personal progress, required are:
+* _valid email address_
+* _available user name_
+
+## _User is also provided with information’s how to:_
+* _set up goals,_
+* _what tools to use,_
+* _useful motivational quotes that are changing dynamically on front page_
+
+## After clicking on exercise tab user have the option to choose exercises for every body part
+### _Searches available by:_
+
+* _name,_
+* _muscle group,_
+* _equipment or_ 
+* _category_
+
+### On workout section user will have available huge variety of workouts that are suitable by his current level.
+
+
 [Deployed Heroku link](https://the-best-workout-app-ever.herokuapp.com/)
--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -8,8 +8,11 @@ class Home extends React.Component {
     this.quotes = [
       '"Never do tomorrow what you can do today. Procrastination is the thief of time." – Charles Dickens',
       "“There are far, far better things ahead than any we leave behind.”– CS Lewis",
-      "Quote 3",
-      "Quote 4",
+      "Of course it’s hard. It’s supposed to be hard. If it were easy, everybody would do it. Hard is what makes it great.",
+      "Your body can stand almost anything. It’s your mind that you have to convince.",
+      '“Well done is better than well said.”- Benjamin Franklin',
+      '"If you want something you’ve never had, you must be willing to do something you’ve never done."- Thomas Jefferson',
+      '"I have nothing in common with lazy people who blame others for their lack of success. Great things come from hard work and perseverance. No excuses." - Kobe Bryant'
     ];
     this.state = { quote: "" };
   }
